d89519bd82cee7830f1030dac7084c3552c6c640
john chishugi <jkchishugi@gmail.com>
2019-08-23 19:12:09 +0200

    fix(Remove unnecessary code ): in file index
    [Finishes #168083795]

--- index.js
+++ index.js
@@ -13,11 +13,6 @@ app.use(express.json());
 //intialize routes vesrion 1
 app.use('/api/v1',routes);
 
-//errors handling middleware
-app.use(function(error,req,res,next){
-  res.status(422).send({url: 'localhost:3000'+req.url ,error:error.message});
-});
-
 app.get('/',(req, res, next) => res.status(200).send({
   status : 200,
   message : 'welcome to the Free mentor Api, below is how to use it',
